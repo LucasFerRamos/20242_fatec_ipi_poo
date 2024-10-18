adfd6302918007191befd20816c913cd319f8218
Lucas <lucaferasp1@gmail.com>
2024-10-18 00:01:31 -0300

    feat:redimensionando e removendo numeros do vetor

--- colecoes/TesteVetorDinamico.java
+++ colecoes/TesteVetorDinamico.java
@@ -4,8 +4,6 @@ import java.util.Random;
 public class TesteVetorDinamico {
     public static void main(String[] args) throws Exception {
      
-     //construir um vetor dinamico de tamanho 8
-     //use o operadpr de inferencia de tipo
         var vetor = new VetorDinamico(8);
         var gerador = new  Random();
         while (true) {
