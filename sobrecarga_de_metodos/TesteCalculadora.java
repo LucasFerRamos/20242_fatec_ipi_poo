f79bfba0f2d091f39fede442599e211c7bde0f91
Aluno <aluno@IPIRANGA.FATEC>
2024-10-31 16:19:42 -0300

    aula(3110):sobrecarga de metodos na calculadora

---
+++ sobrecarga_de_metodos/TesteCalculadora.java
@@ -0,0 +1,10 @@
+package  sobrecarga_de_metodos;
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        var calc = new Calculadora();
+        System.out.println(calc.somar(2, 3));
+        System.out.println(calc.somar(5.5, 3.5));
+        System.out.println(calc.somar(5, "5"));
+        System.out.println(calc.somar("5", 6));
+    }
+}
